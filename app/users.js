31cb29872ba1b737bd3746ff98223988e8f03828
Fernando <fernandoza@gmail.com>
2017-06-19 16:33:38 -0500

    - Syntax error in the map and filter implementation.

--- app/users.js
+++ app/users.js
@@ -1,24 +1,49 @@
 /**
  * Created by fer on 6/19/17.
  */
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+var PropTypes = require('prop-types');
+
 class Users extends React.Component {
     render() {
+
+        var friends = this.props.list.filter(function (user){
+            return user.friend === true;
+        });
+
+        var nonFriends = this.props.list.filter(function (user){
+            return user.friend !== true;
+        });
+
         return (
             <div>
                 <h1>Friends</h1>
                 <ul>
-                    {/*Create an <li> for every name in the list array who is also your friend*/}
+                    {friends.map(function (friend){
+                        return <li>{friend.name}</li>
+                    })};
                 </ul>
 
                 <hr />
 
                 <h1> Non Friends </h1>
                 <ul>
-                    {/*Create an <li> for every name in the list array who is NOT your friend*/}
+                    {nonFriends.map(function (friendWho){
+                        return <li>{friendWho.name}</li>
+                    })};
                 </ul>
             </div>
         )
-    }
+
+    }// render end
+}
+
+// Makes the list required and always expect an array when you try to use it.
+
+User.propTypes = {
+    list: PropTypes.array.isRequired
 }
 
 ReactDOM.render(
