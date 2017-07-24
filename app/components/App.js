82d64231e7c381ea762be2bc5c515b096842de6f
Fernando <fernandoza@gmail.com>
2017-07-24 14:36:53 -0500

    - Added the react router - Implemented the router for the main menu - Created the nav and home components.

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,13 @@
 /**
  * Created by fer on 7/4/17.
  */
-var React = require('react');
-var Popular = require('./Popular');
+var React = require("react");
+var Popular = require("./Popular");
+var ReactRouter = require("react-router-dom");
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require("./Nav");
+var Home = require("./Home");
 
 // Adding an example of a Stateless functional component
 function Welcome(props){
@@ -14,10 +19,14 @@ function Welcome(props){
 class App extends React.Component{
     render(){
         return (
-            <div className="container">
-                <Welcome username="Fer" />
-                <Popular />
-            </div>
+            <Router>
+                <div className="container">
+                    <Welcome username="Fer" />
+                    <Nav />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/popular" component={Popular} />
+                </div>
+            </Router>
         )
     }
 }
