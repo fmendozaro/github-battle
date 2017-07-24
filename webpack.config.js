7522711a22bb54d632a381ac0f74744d0770ac97
Fernando <fernandoza@gmail.com>
2017-07-24 14:44:13 -0500

    - Added a devServer entry for the webconfig file. This takes care of the get requests from the browser.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	entry: {
-		index: './app/index.js',
-		users: './app/users.js'
+		index: "./app/index.js",
+		users: "./app/users.js"
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name]_bundle.js'
-	},
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name]_bundle.js",
+        publicPath: "/"
+    },
 	module: {
 		rules: [
-			{test: /\.(js)$/, use: 'babel-loader'},
-			{test: /\.css$/, use: ['style-loader', 'css-loader']}
+			{test: /\.(js)$/, use: "babel-loader"},
+			{test: /\.css$/, use: ["style-loader", "css-loader"]}
 
 		]
 	},
-	plugins: [
+    devServer: {
+		historyApiFallback: true
+	},
+    plugins: [
 		new HtmlWebpackPlugin({
-            template: 'app/index.html'
+            template: "app/index.html"
         })]
 }
