ae1e9d36cc2926a52e75cc8b09452162ed4b7f71
Fernando <fernandoza@gmail.com>
2017-09-07 17:16:51 -0500

    Complete the complete promises exercise

--- app/utils/ES6/promises.js
+++ app/utils/ES6/promises.js
@@ -1,4 +1,7 @@
 // Promises
+
+var optionalHeaders = {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}}
+
 function wait(millisecs){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -7,5 +10,20 @@ function wait(millisecs){
     })
 }
 
+function getLastCommit(username){
+    return fetch("https://api.github.com/users/"+username+"/events").then((response) => {
+        return response.json();
+    }).catch( error => {
+        console.log(error);
+    });
+}
+
 wait(1000).then( () => { console.log(`You'll see this after 1 second`) });
 wait(3000).then( () => { console.log(`You'll see this after 3 seconds`) });
+
+var promise = getLastCommit("fmendozaro");
+
+// prints the date of the last commit
+promise.then(commits => {
+  console.log(commits[0].created_at);
+});
